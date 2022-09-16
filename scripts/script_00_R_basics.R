94aea1ddd204acbbc748c491edb61120dcec9e6a
Keith Lohse <lohse@wustl.edu>
2022-09-16 15:02:07 -0500

    Adding R Basics Script
    Adding first draft of R basics script. Finishing data visualization script.

---
+++ scripts/script_00_R_basics.R
@@ -0,0 +1,13 @@
+# assigning values to variable
+
+# data types 
+
+# functions and arguments
+
+# installing and loading packages
+
+# setting the working directory
+
+# importing data from your computer
+
+# importing data from the web
--- scripts/script_03_data_visualization.R
+++ scripts/script_03_data_visualization.R
@@ -165,14 +165,15 @@ ggplot(data=DAT_COND_LONG_LOG, aes(x=ln_Hz, y=ln_power)) +
 
 # Spaghetti Plots layering individual participant data with aggregate data:
 head(DATA)
-
 head(DAT_COND_AVE)
-ggplot(data=DAT_COND_LONG_LOG, aes(x=ln_Hz, y=ln_power)) +
-  geom_line(aes(col=region)) +
-  geom_point(aes(col=region), shape=16) +
+
+ggplot(data=DATA, aes(x=ln_Hz, y=ln_occipital)) +
+  geom_line(aes(group=subID, col=subID), alpha=0.5) +
   facet_wrap(~condition)+
-  scale_x_continuous(name = "Frequency log(Hz)") +
-  scale_y_continuous(name = "Power log(uV^2)") +
+  geom_line(data=DAT_COND_AVE, aes(x=ln_Hz, y=ln_occipital), 
+            col="black", lwd=1) +
+  scale_x_continuous(name = "Frequency (Hz)") +
+  scale_y_continuous(name = "Power (uV^2)") +
   theme_bw()+
   labs(color="Region")+
   theme(axis.text=element_text(size=10, color="black"), 
@@ -182,4 +183,67 @@ ggplot(data=DAT_COND_LONG_LOG, aes(x=ln_Hz, y=ln_power)) +
         plot.title=element_text(size=12, face="bold", hjust=0.5),
         panel.grid.minor = element_blank(),
         strip.text = element_text(size=12, face="bold"),
-        legend.position = "bottom")
+        legend.position = "none")
+
+
+
+# plot of the differences between EO and EC conditions
+head(DATA)
+?str_sub
+DAT_OCCIP_WIDE <- DATA %>% mutate(group=str_sub(subID, start=1,end=2)) %>%
+  select(subID, condition,  group, Hz, occipital, ln_Hz, ln_occipital) %>%
+  pivot_wider(names_from=condition, values_from = c(occipital, ln_occipital))
+
+head(DAT_OCCIP_WIDE)
+
+DAT_OCCIP_WIDE$diff_ln_power <- DAT_OCCIP_WIDE$ln_occipital_ec - DAT_OCCIP_WIDE$ln_occipital_eo
+
+
+ggplot(data=DAT_OCCIP_WIDE, aes(x=ln_Hz, y=diff_ln_power)) +
+  geom_line(aes(group=subID, col=subID), alpha=0.5) +
+  facet_wrap(~group)+
+  # geom_line(data=DAT_COND_AVE, aes(x=ln_Hz, y=ln_occipital), 
+  #           col="black", lwd=1) +
+  scale_x_continuous(name = "Frequency (Hz)") +
+  scale_y_continuous(name = "Power (uV^2)") +
+  theme_bw()+
+  labs(color="Region")+
+  theme(axis.text=element_text(size=10, color="black"), 
+        legend.text=element_text(size=12, color="black"),
+        legend.title=element_text(size=12, face="bold"),
+        axis.title=element_text(size=12, face="bold"),
+        plot.title=element_text(size=12, face="bold", hjust=0.5),
+        panel.grid.minor = element_blank(),
+        strip.text = element_text(size=12, face="bold"),
+        legend.position = "none")
+
+
+# Calculating the group mean differences:
+head(DAT_OCCIP_WIDE)
+
+DAT_OCCIP_WIDE_GROUP_AVE <- DAT_OCCIP_WIDE %>% group_by(group, Hz) %>%
+  summarize(ln_Hz = ln_Hz[1],
+            diff_ln_power=mean(diff_ln_power, na.rm=TRUE))
+
+head(DAT_OCCIP_WIDE_GROUP_AVE)
+
+
+ggplot(data=DAT_OCCIP_WIDE, aes(x=ln_Hz, y=diff_ln_power)) +
+  geom_line(aes(group=subID, col=subID), alpha=0.5) +
+  facet_wrap(~group)+
+  geom_line(data=DAT_OCCIP_WIDE_GROUP_AVE, 
+            aes(x=ln_Hz, y=diff_ln_power), 
+             col="black", lwd=1) +
+  scale_x_continuous(name = "Frequency (Hz)") +
+  scale_y_continuous(name = "Power (uV^2)") +
+  theme_bw()+
+  labs(color="Region")+
+  theme(axis.text=element_text(size=10, color="black"), 
+        legend.text=element_text(size=12, color="black"),
+        legend.title=element_text(size=12, face="bold"),
+        axis.title=element_text(size=12, face="bold"),
+        plot.title=element_text(size=12, face="bold", hjust=0.5),
+        panel.grid.minor = element_blank(),
+        strip.text = element_text(size=12, face="bold"),
+        legend.position = "none")
+
