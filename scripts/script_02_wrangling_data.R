2681b2a6e591f1360389465be6d00704fa1e0d71
Keith Lohse <lohse@wustl.edu>
2022-09-22 11:33:16 -0500

    Updates to wrangling data
    Updated wrangling data script following recording.

--- scripts/script_02_wrangling_data.R
+++ scripts/script_02_wrangling_data.R
@@ -22,21 +22,24 @@ DATA %>% select(subID:F3)
 DATA %>% select(-X)
 
 DAT2 <- DATA %>% select(-X, -file_id)
-
+head(DAT2)
 
 # filtering specific rows
 head(DAT2)
 ?dplyr::filter
-DAT2 %>% filter(subID=="oa01")
+DAT3 <- DAT2 %>% filter(subID=="oa01")
 
-DAT2 %>% filter(subID=="oa01" | subID=="oa02")
+DAT3<- DAT2 %>% filter(subID=="oa01" | subID=="oa02")
 
-DAT2 %>% filter(subID=="oa01" & Hz==0.997)
+DAT3 <- DAT2 %>% filter(subID=="oa01" & Hz==0.997)
+DAT3
 
 summary(unique(DAT2$Hz))
 hist(unique(DAT2$Hz))
 
 DAT3 <- DAT2 %>% filter(Hz<=30)
+summary(unique(DAT3$Hz))
+hist(unique(DAT3$Hz))
 
 
 # computing new variables
@@ -47,14 +50,15 @@ DAT3$Frontal <- (DAT3$F3 + DAT3$F7 + DAT3$Fz + DAT3$F4 + DAT3$F8)/5
 ?dplyr::mutate()
 ?dplyr::transmute()
 
-? dplyr::rowwise
+?dplyr::rowwise
 DAT3 <- DAT3 %>% rowwise %>%
   mutate(frontal = mean(c(F3, F7, Fz, F4, F8), na.rm=TRUE),
-         central = mean(c(C3, Pz, C4), na.rm=TRUE),
-         parietal = mean(c(P3, P7, Fz, P4, P8), na.rm=TRUE),
+         central = mean(c(C3, Cz, C4), na.rm=TRUE),
+         parietal = mean(c(P3, P7, Pz, P4, P8), na.rm=TRUE),
          occipital = mean(c(O1, Oz, O2), na.rm=TRUE)
   )
 
+head(DAT3)
 plot(DAT3$Frontal, DAT3$frontal)
 cor(DAT3$Frontal, DAT3$frontal, use = "complete.obs")
 
@@ -64,7 +68,7 @@ cor(DAT3$Frontal, DAT3$frontal, use = "complete.obs")
 head(DAT3)
 DAT4 <- DAT3 %>% select(subID, condition, Hz,
                         frontal, central, parietal, occipital) %>%
-  mutate(ln_Hz = log(Hz+1),
+  mutate(ln_Hz = log(Hz),
          ln_frontal = log(frontal),
          ln_central = log(central),
          ln_parietal = log(parietal),
