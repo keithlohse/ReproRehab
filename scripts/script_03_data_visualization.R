02f28d0b33d9a62daddddd37b0950a3a29efda62
Keith Lohse <lohse@wustl.edu>
2022-09-23 10:58:00 -0500

    Update script_03_data_visualization.R

--- scripts/script_03_data_visualization.R
+++ scripts/script_03_data_visualization.R
@@ -1,7 +1,7 @@
 library(tidyverse) # data formatting and graphing tools
 
 
-# 2.0. Wrangling Data 
+# 3.0. Data Visualization 
 setwd("~/GitHub/ReproRehab/")
 list.files()
 list.files("./data/")
@@ -31,16 +31,16 @@ ggplot(data=DAT_GRAND_AVE, aes(x=Hz, y=frontal)) +
   geom_point(shape=16, col="black") +
   scale_x_continuous(name = "Frequency (Hz)") +
   scale_y_continuous(name = "Power (uV^2)") +
-  ggtitle("Frontal Power")+
-  theme_bw()+
+  ggtitle("Frontal Power") +
+  theme_bw() +
   theme(axis.text=element_text(size=10, color="black"), 
-        legend.text=element_text(size=12, color="black"),
-        legend.title=element_text(size=12, face="bold"),
-        axis.title=element_text(size=12, face="bold"),
-        plot.title=element_text(size=12, face="bold", hjust=0.5),
-        panel.grid.minor = element_blank(),
-        strip.text = element_text(size=12, face="bold"),
-        legend.position = "bottom")
+         legend.text=element_text(size=12, color="black"),
+         legend.title=element_text(size=12, face="bold"),
+         axis.title=element_text(size=12, face="bold"),
+         plot.title=element_text(size=12, face="bold", hjust=0.5),
+         panel.grid.minor = element_blank(),
+         strip.text = element_text(size=12, face="bold"),
+         legend.position = "bottom")
 
 
 ggplot(data=DAT_GRAND_AVE, aes(x=ln_Hz, y=ln_frontal)) +
@@ -69,7 +69,7 @@ head(DAT_GRAND_AVE_LONG)
 
 DAT_GRAND_AVE_LOG <- DAT_GRAND_AVE %>% select(ln_Hz:ln_occipital) %>%
   pivot_longer(cols=ln_frontal:ln_occipital, names_to = "region", values_to = "ln_power")
-head(DAT_GRAND_AVE_LONG)
+head(DAT_GRAND_AVE_LOG)
 
 
 ggplot(data=DAT_GRAND_AVE_LONG, aes(x=Hz, y=power)) +
@@ -172,8 +172,8 @@ ggplot(data=DATA, aes(x=ln_Hz, y=ln_occipital)) +
   facet_wrap(~condition)+
   geom_line(data=DAT_COND_AVE, aes(x=ln_Hz, y=ln_occipital), 
             col="black", lwd=1) +
-  scale_x_continuous(name = "Frequency (Hz)") +
-  scale_y_continuous(name = "Power (uV^2)") +
+  scale_x_continuous(name = "Frequency, ln(Hz)") +
+  scale_y_continuous(name = "Power,  ln(uV^2)") +
   theme_bw()+
   labs(color="Region")+
   theme(axis.text=element_text(size=10, color="black"), 
@@ -202,8 +202,8 @@ DAT_OCCIP_WIDE$diff_ln_power <- DAT_OCCIP_WIDE$ln_occipital_ec - DAT_OCCIP_WIDE$
 ggplot(data=DAT_OCCIP_WIDE, aes(x=ln_Hz, y=diff_ln_power)) +
   geom_line(aes(group=subID, col=subID), alpha=0.5) +
   facet_wrap(~group)+
-  # geom_line(data=DAT_COND_AVE, aes(x=ln_Hz, y=ln_occipital), 
-  #           col="black", lwd=1) +
+  geom_line(data=DAT_COND_AVE, aes(x=ln_Hz, y=ln_occipital), 
+             col="black", lwd=1) +
   scale_x_continuous(name = "Frequency (Hz)") +
   scale_y_continuous(name = "Power (uV^2)") +
   theme_bw()+
@@ -217,6 +217,7 @@ ggplot(data=DAT_OCCIP_WIDE, aes(x=ln_Hz, y=diff_ln_power)) +
         strip.text = element_text(size=12, face="bold"),
         legend.position = "none")
 
+head(DAT_COND_AVE)
 
 # Calculating the group mean differences:
 head(DAT_OCCIP_WIDE)
@@ -234,8 +235,8 @@ ggplot(data=DAT_OCCIP_WIDE, aes(x=ln_Hz, y=diff_ln_power)) +
   geom_line(data=DAT_OCCIP_WIDE_GROUP_AVE, 
             aes(x=ln_Hz, y=diff_ln_power), 
              col="black", lwd=1) +
-  scale_x_continuous(name = "Frequency (Hz)") +
-  scale_y_continuous(name = "Power (uV^2)") +
+  scale_x_continuous(name = "Frequency, ln(Hz)") +
+  scale_y_continuous(name = "Change in Log Power (EC-EO)") +
   theme_bw()+
   labs(color="Region")+
   theme(axis.text=element_text(size=10, color="black"), 
